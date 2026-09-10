caf6ab3fc879d807db4706e1f9861a4f87006b1c
Sufyan <44521060+sufyanAshraf@users.noreply.github.com>
2026-09-10 13:08:35 +0500

    Implement dataBase class for Pinecone integration with index creation functionality

---
+++ app/dataBase.py
@@ -0,0 +1,44 @@
+from pinecone import Pinecone, ServerlessSpec 
+
+from .logger import logger
+
+
+class dataBase:
+    # _instance = None
+
+    # def __new__(cls):
+    #     if cls._instance is None:
+    #         cls._instance = super().__new__(cls)
+    #     return cls._instance
+
+    def __init__(self, key):
+        # if not hasattr(self, "index"):
+        #     self.index = None
+        self.key = key
+        self.pc = Pinecone(api_key=self.key)
+
+        logger.info("Successfully initilize")
+
+    def create_index(self, index_name):
+        index_flag = False
+        try:
+        
+            if not self.pc.has_index(index_name):
+                self.pc.create_index_for_model(
+                    name=index_name,
+                    cloud="aws",
+                    region="us-east-1",
+                    embed={
+                        "model":"llama-text-embed-v2",
+                        "field_map":{"text": "chunk_text"}
+                    }
+                )
+                logger.info("Index created")
+            else:
+                logger.info("Index already exist")
+                index_flag = True
+        except Exception as e:
+            logger.info("Index fail")
+            raise 
+
+        return self.pc, index_flag
